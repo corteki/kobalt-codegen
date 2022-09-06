ab6c6f853dc110366193b788ccad92586d1c53af
Tim Alaerts <timalaerts1990@gmail.com>
2022-09-06 13:31:12 +0200

    added prettier to format the strings generated

--- src/modules/templates.ts
+++ src/modules/templates.ts
@@ -1,10 +1,11 @@
-import { indent } from "../utilities/format";
+import { format } from "prettier";
 
 export const createStyledComponentFont = (name: string, font: string) =>
   `export const ${name} = styled.p\`${font}\`;\n`;
 
 export const createTemplate = (body: string) =>
-  indent(`
+  format(
+    `
   import type { ReactNode } from "react";
   import styled, { ThemeProvider } from "styled-components";
   import { theme } from "./theme";
@@ -16,19 +17,27 @@ export const createTemplate = (body: string) =>
   export const GeneratedTheme = ({ children }: GeneratedThemeProps) => (
     <ThemeProvider theme={theme}>{children}</ThemeProvider>
   );
-  `);
+  `,
+    { parser: "typescript" }
+  );
 
 export const createTheme = (tokens: string) =>
-  indent(`
+  format(
+    `
 export const theme = ${tokens};
 export type Theme = typeof theme;
-`);
+`,
+    { parser: "typescript" }
+  );
 export const createThemeDeclaration = () =>
-  indent(`
+  format(
+    `
 import type { Theme } from "./${process.env["OUTPUT_FOLDER"]}/theme";
 import "styled-components";
 
 declare module "styled-components" {
   interface DefaultTheme extends Theme {}
 }
-`);
+`,
+    { parser: "typescript" }
+  );
